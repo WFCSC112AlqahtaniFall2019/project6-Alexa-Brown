4a9ab8f945a33710dc4c221adb57c03fd630b394
Alexa-Brown <browam18@wfu.edu>
2019-10-24 16:44:15 -0400

    working on the project

---
+++ LinkedList.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "Node.h"
+#include "LinkedList.h"
+using namespace std;
+
+LinkedList::LinkedList() {
+    Node(0, nullptr);
+}
+
+LinkedList::~LinkedList(){
+
+}
+
+void LinkedList::append(int item) {
+    head = nullptr;
+    if (head){
+        head->next = new Node(item);
+    }
+    else {
+        Node* current = head;
+        while (current->next != nullptr){
+            current = current->next;
+            current->next = new Node(item);
+        }
+    }
+}
+
+void LinkedList::printList() const{
+   Node* current = head;
+   if(! current){
+       cout << "Empty list" << endl;
+   }
+   else{
+       while (current){
+           cout << current->value << endl;
+           current = current->next;
+       }
+   }
+}
